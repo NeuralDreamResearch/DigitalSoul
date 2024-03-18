1fb7fc1ee1d64e9e349c99a269a6c568871372ae
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2024-03-18 22:40:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, Extension, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 import pybind11
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 # 1. C++/Pybind11 Extension
 dscpp_ext = Pybind11Extension(
     'DigitalSoul.dscpp',
@@ -13,13 +16,24 @@ dscpp_ext = Pybind11Extension(
 
 # 2. Find Pure Python Packages
 packages = find_packages()  # Automatically find 'DigitalSoul'
-print("Found packages:", packages)  # Add this line
 setup(
-    name='DigitalSoul',  # Name of your overall project
-    version='1.0.0',
-    description='My DigitalSoul project',
+    name='DigitalSoul', 
+    version='1.1.0',
+    description='Unified Compute Platform - CPU, GPU, FPGA, Quantum Computing',
     packages=packages,
     ext_modules=[dscpp_ext],
-    cmdclass={'build_ext': build_ext}  # Ensure build_ext is used for compilation
+    cmdclass={'build_ext': build_ext}, 
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/NeuralDreamResearch/DigitalSoult',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        
+    ],
 )
 
